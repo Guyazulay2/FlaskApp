2a05b419f414ec081d8bec0941ea42aaa574eeb9
guyazulay2 <guykort98@gmail.com>
2021-07-12 10:27:32 +0300

    Add new files

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ import random
 app = Flask(__name__, template_folder='../flaskk')
 from flask import (Flask, render_template, request, redirect, url_for, session)
 
-@app.route("/home/<name>")
-def login(name):
-    return f"<h1> Hi : {name}<h1>"
-
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if request.method == "POST":
@@ -22,23 +18,38 @@ def main():
             return Lottery(body)
 
     elif request.method == "GET":
-        print(request.args)
+        # print(request.args)
         if 'username' in request.args:
             name = request.args['username']
         if 'password' in request.args:
             passs = request.args['password']
-            print("Your Email :",name,"\nYour Password :",passs, file=open('C:/Users/USER/Desktop/VSCODE/flaskk/details.txt', 'w'))  
+            print("Your Email :",name,"\nYour Password :",passs, file=open('C:/Users/USER/Desktop/VSCODE/flaskk/details.txt', 'w'))
             if name == '' or passs == '':
                 return render_template('return.html')
             else:  
-                return redirect('/home')
-                
-        return render_template('login.html')
+                return redirect('/menu')
 
+        return render_template('login.html')
 
 @app.route('/home')
 def home_page():
+    # print(request.args)
+    if 'menu' in request.args:
+        return redirect('/menu')
     return render_template('home.html')
+
+@app.route('/menu')
+def menu():
+    # print(request.args)
+    if 'home1' in request.args:
+        return redirect('/home')
+    elif 'home2' in request.args:
+        return render_template('home2.html')
+    elif 'home3' in request.args:
+        return render_template('home3.html')
+    elif 'login' in request.args:
+        return redirect('/')
+    return render_template('menu.html')
 
 
 def Details(body):
@@ -68,10 +79,6 @@ def Lottery(body):
         return f"The number Was :{num} \n You Win !"
     else:
         return f"The Number Was :{num} \n You Lose !"
-
-@app.route("/logout")
-def Logout():
-    return 'Logout, ByeBye !'
 
 # parser = argparse.ArgumentParser()
 # parser.add_argument("name", type= str, help="Enter Ip")
